dbc7c793a9d9bde2053290430b0e35b40db51eae
Alexander Johnston <Aliics@hotmail.com>
2024-01-27 18:13:23 +1300

    Code organization, styles, and validate guesses.

--- src/main/scala/openredactle/Guesses.scala
+++ src/main/scala/openredactle/Guesses.scala
@@ -5,42 +5,70 @@ import com.raquo.laminar.api.L.{*, given}
 case class Guess(word: String)
 
 object Guesses:
-  private val guessedWords: Var[Set[Guess]] = Var(Set())
+  private val guessedWords: Var[List[Guess]] = Var(List())
 
-  private def addGuess(guess: Guess): Unit =
-    guessedWords.update(_ + guess)
+  def renderElement: Element =
+    div(
+      width := "16rem",
+      height := "100%",
+      display := "flex",
+      flexDirection := "column",
+      borderLeft := "solid 1px black",
+
+      renderGuessForm,
+
+      div(
+        flexGrow := 1,
+        overflowY := "scroll",
+
+        children <-- guessedWords.signal
+          .splitByIndex(renderGuess)
+          .map(_.reverse),
+      ),
+    )
+
+  private def renderGuessForm: Element =
+    val guessInput = Var("")
+
+    form(
+      display := "flex",
+      padding := "1rem",
+      borderBottom := "solid 1px black",
+
+      onSubmit
+        .preventDefault
+        .mapTo(guessInput.now()) --> submitGuess,
+
+      input(
+        flexGrow := 1,
+        value <-- guessInput,
+
+        onInput.mapToValue --> guessInput,
+      ),
+      button("Guess"),
+    )
+
+  private def submitGuess(input: String): Unit =
+    val guess = Guess(input)
+    guessedWords.update: guesses =>
+      if guess.word.isBlank || guesses.contains(guess) then guesses
+      else guesses :+ guess
 
   private def renderGuess(index: Int, initial: Guess, guessSignal: Signal[Guess]): Element =
     div(
-      span(index + 1),
-      child.text <-- guessSignal.map(_.word),
-    )
+      display := "flex",
+      alignItems := "center",
+      borderBottom := "solid 1px black",
+      minWidth := "fit-content",
+      fontSize := "14px",
+      padding := "0.25rem",
 
-  def renderElement: Element =
-    val guessInput = Var("")
+      span(
+        fontSize := "13px",
+        color := "grey",
+        marginRight := "0.5rem",
 
-    div(
-      width := "20rem",
-
-      form(
-        width := "100%",
-        display := "flex",
-
-        onSubmit
-          .preventDefault
-          .mapTo(guessInput.now()) --> (w => addGuess(Guess(w))),
-
-        input(
-          flexGrow := 1,
-          value <-- guessInput,
-          onInput.mapToValue --> guessInput,
-        ),
-        button("Guess"),
+        index + 1,
       ),
-
-      hr(),
-
-      children <-- guessedWords.signal
-        .splitByIndex(renderGuess)
-        .map(_.toSeq.reverse)
+      child.text <-- guessSignal.map(_.word),
     )
