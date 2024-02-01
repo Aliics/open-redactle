6b3ff7254abe5e92adaa0426f786d41c1bd9fcb8
Alexander Johnston <Aliics@hotmail.com>
2024-02-02 08:22:55 +1300

    Fix test expectations for spaces.

--- server/src/test/scala/openredactle/server/data/wordTest.scala
+++ server/src/test/scala/openredactle/server/data/wordTest.scala
@@ -6,32 +6,32 @@ import org.scalatest.matchers.should.Matchers
 
 class wordTest extends AnyFunSuite with Matchers:
   test("one word should create one Known Word"):
-    wordsFromString("bunny") shouldBe Seq(Word.Known("bunny"))
+    wordsFromString("bunny") shouldBe Seq(Word.Known("bunny", true))
 
   test("many words should create multiple Known Words"):
-    wordsFromString("bunnies are awesome") shouldBe Seq(Word.Known("bunnies"), Word.Known("are"), Word.Known("awesome"))
+    wordsFromString("bunnies are awesome") shouldBe Seq(Word.Known("bunnies", true), Word.Known("are", true), Word.Known("awesome", true))
 
   test("word with full stop should create Word and Punctuation"):
-    wordsFromString("spuds.") shouldBe Seq(Word.Known("spuds"), Word.Punctuation('.'))
+    wordsFromString("spuds.") shouldBe Seq(Word.Known("spuds", false), Word.Punctuation('.', true))
 
   test("multiple sentences should create a complex series of Words"):
     wordsFromString("that is... because spuds are amazing, and you should know it! number 2") shouldBe Seq(
-      Word.Known("that"),
-      Word.Known("is"),
-      Word.Punctuation('.'),
-      Word.Punctuation('.'),
-      Word.Punctuation('.'),
-      Word.Known("because"),
-      Word.Known("spuds"),
-      Word.Known("are"),
-      Word.Known("amazing"),
-      Word.Punctuation(','),
-      Word.Known("and"),
-      Word.Known("you"),
-      Word.Known("should"),
-      Word.Known("know"),
-      Word.Known("it"),
-      Word.Punctuation('!'),
-      Word.Known("number"),
-      Word.Known("2"),
+      Word.Known("that", true),
+      Word.Known("is", false),
+      Word.Punctuation('.', false),
+      Word.Punctuation('.', false),
+      Word.Punctuation('.', true),
+      Word.Known("because", true),
+      Word.Known("spuds", true),
+      Word.Known("are", true),
+      Word.Known("amazing", false),
+      Word.Punctuation(',', true),
+      Word.Known("and", true),
+      Word.Known("you", true),
+      Word.Known("should", true),
+      Word.Known("know", true),
+      Word.Known("it", false),
+      Word.Punctuation('!', true),
+      Word.Known("number", true),
+      Word.Known("2", true),
     )
